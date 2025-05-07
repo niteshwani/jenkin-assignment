2d47caddee7d32903b2750dfd16a4c8b3ac58314
niteshwani <niteshwani2@gmail.com>
2025-05-07 19:45:26 +0530

    Adding welcome to ditiss

--- HelloStudent.java
+++ HelloStudent.java
@@ -2,5 +2,6 @@ public class HelloStudent {
     public static void main(String[] args) {
         System.out.println("Hello, Nitesh Wani"); 
 	System.out.println("Welcome to C-DAC");
+	System.out.println("Welcome to DITISS");
     }
 }
